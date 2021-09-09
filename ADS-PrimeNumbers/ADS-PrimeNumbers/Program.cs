576c59ad9d82d15f74220d52365dea43186b6ca0
Karolina Kotlarz <kotlarzkarolina16@gmail.com>
2021-09-09 18:27:59 -0400

    Build Project

---
+++ ADS-PrimeNumbers/ADS-PrimeNumbers/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ADS_PrimeNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
